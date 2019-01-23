d9085d5de4c971541a1d5a06f79b9b0ffd545a2a
Daniel Babiak <daniel.babiak8@gmail.com>
2019-01-23 17:01:29 +0100

    no message

---
+++ app/src/main/java/students/com/movierecommender/view/MovieActivity.java
@@ -0,0 +1,21 @@
+package students.com.movierecommender.view;
+
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import students.com.movierecommender.R;
+
+import java.util.ArrayList;
+
+public class MovieActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie);
+
+    }
+}
